7ac1dca9ad46520338d781269a0223a6d999f2be
PauliusK-bit <kareiva.paulius13@gmail.com>
2025-09-17 20:24:03 +0300

    bug fixed

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,42 +1,9 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const Item = require("../models/itemModel");
 
-// const getCheckout = async (req, res) => {
-//   console.log("req.params:", req.params);
-
-//   const item = await Item.findById(req.params.itemId);
-
-//   const paymentIntent = await stripe.paymentIntents.create({
-//     // mode: "payment",
-//     // payment_method_types: ["card"],
-//     // success_url: `${req.protocol}://${req.get("host")}`,
-//     // cancel_url: `${req.protocol}://${req.get("host")}`,
-
-//     // client_reference_id: req.params.itemId,
-//     // line_items: [
-//     //   {
-//     //     price_data: {
-//     //       currency: "eur",
-//     //       product_data: {
-//     //         name: `${item.name} Product`,
-//     //         description: item.summary,
-//     //         images: [],
-//     //       },
-//     //       unit_amount: Math.round(item.price * 100),
-//     //     },
-//     //     quantity: 1,
-//     //   },
-//     // ],
-
-//     amount: Math.round(item.price * 100),
-//     currency: "eur",
-//   });
-
-//   res.status(200).json({
-//     status: "success",
-//     clientSecret: paymentIntent.client_secret,
-//   });
-// };
+const generateOrderId = () => {
+  return `order_${Date.now()}_${Math.random().toString(36).substring(2, 7)}`;
+};
 
 const calculateOrderAmount = (items) => {
   let total = 0;
